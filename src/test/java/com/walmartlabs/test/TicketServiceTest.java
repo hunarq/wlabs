425c3dab3095b62bff626e02246272bfd467005e
Hunar <hunarq@gmail.com>
2018-06-15 12:03:51 -0400

    added more tests asserts

--- src/test/java/com/walmartlabs/test/TicketServiceTest.java
+++ src/test/java/com/walmartlabs/test/TicketServiceTest.java
@@ -25,26 +25,35 @@ public class TicketServiceTest {
 		
 	}
 
+	/// test NumSeatsAvailable
 	@Test
 	public final void testNumSeatsAvailable() {
 		TicketService ts = new TicketServiceImpl();
 		assertEquals(ts.numSeatsAvailable(), 20);
 	}
 
+	/// test FindAndHoldSeats
 	@Test
 	public final void testFindAndHoldSeats() {
 		TicketService ts = new TicketServiceImpl();
 		SeatHold sh = ts.findAndHoldSeats(20, "test@test.com");
-		assertTrue(sh.getSeats().length == 20);
+		assertTrue(sh.getSeatsHeld() == 20);
+		
+		sh = ts.findAndHoldSeats(20, "test@test.com");
+		assertTrue(sh.getSeatsHeld() == 0);
+		
 	}
 
+	/// test reserveSeats
 	@Test
 	public final void testReserveSeats() {
 		TicketService ts = new TicketServiceImpl();
 		SeatHold sh = ts.findAndHoldSeats(20, "test@test.com");
 		String confirmCode = ts.reserveSeats(sh.getId(), "test@test.com");
 		assertNotNull(confirmCode);
-	}
 
+		confirmCode = ts.reserveSeats(100, "test@test.com");
+		assertNull(confirmCode);
+	}
 
 }
